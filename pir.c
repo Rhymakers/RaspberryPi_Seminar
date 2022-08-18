5a93757208aa50a8065fde0d29a734ce640f6830
Jonghee Son <jhson@paperecord.com>
2022-08-18 21:27:28 +0900

    Added example code for PIR sensor

---
+++ pir.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <wiringPi.h>
+
+#define PIRpin 7
+
+int main() {
+    /*
+    Example code for PIR sensor
+    When PIR sensor detects something, this code outputs result as console output
+    */
+    wiringPiSetup();
+
+    pinMode(PIRpin, INPUT);
+
+    for (;;) {
+        //PIR sensor omits result as 1 (HIGH) & 0 (LOW)
+        if (digitalRead(PIRpin) == HIGH) {
+            printf("Detected\n");
+
+            delay(60);
+        }
+        else if (digitalRead(PIRpin) == LOW) {
+            printf("Not Detected\n");
+
+            delay(60);
+        }
+        else {
+            printf("Error\n");
+            break;
+        }
+    }
+
+    return 0;
+}
