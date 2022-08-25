747f770da495c14025b1ca12986ea5fc695f02bb
syh <songyong2535@naver.com>
2022-08-25 22:00:10 +0900

    강의용 LED_blink 코드 수정

--- 1st Training/led_test.c
+++ 1st Training/led_test.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-#include <wiringPi.h>
+#include <stdio.h>		// c언어 표준입출력 라이브러리
+#include <wiringPi.h>	// 라즈베리파이에서 gpio 제어를 위한 라이브러리
 
-#define LED 1
-#define SWITCH 4
+#define LED 1			// LED의 + 극에 연결된 라즈베리파이 핀
+#define BUTTON 4		// BUTTON에 연결된 라즈베리파이 핀
 
 int main() {
 	/*
-	Example code for controlling LED with hardware switch
+	Example code for controlling LED with hardware button
 	Outputs result as console output
 	*/
 	if(wiringPiSetup() == -1) {
@@ -15,12 +15,12 @@ int main() {
 	//initializing wiringPi for GPIO control
 	//uses wiringpi pinmap
 
-	pinMode(LED, OUTPUT); //set "LED" pin as OUTPUT
-	pinMode(SWITCH, INPUT); //set "SWITCH" pin as INPUT
+	pinMode(LED, OUTPUT); 	//set "LED" pin as OUTPUT
+	pinMode(BUTTON, INPUT); //set "BUTTON" pin as INPUT
 	
 	while(1) {
-		if(digitalRead(SWITCH) == HIGH) {
-			//read input from switch and if it is HIGH, turns on connected LED
+		if(digitalRead(BUTTON) == HIGH) {
+			//read input from button and if it is HIGH, turns on connected LED
 			printf("Button pressed\n");
 			digitalWrite(LED, HIGH);
 		}
