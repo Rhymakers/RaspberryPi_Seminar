9bfb5e1337006826ef74aa674445bd6fe3ee11c2
syh <songyong2535@naver.com>
2022-08-25 21:46:03 +0900

    강의용 인터럽트코드 수정

--- 2nd Training/interrupt_2.c
+++ 2nd Training/interrupt_2.c
@@ -1,17 +1,19 @@
-#include <stdio.h>
-#include <wiringPi.h>
+#include <stdio.h>		// c언어 표준입출력 라이브러리
+#include <wiringPi.h>	// 라즈베리파이에서 gpio 제어를 위한 라이브러리
 
-#define RED_LED 23
-#define YELLOW_LED 24
-#define GREEN_LED 25
-#define BTN_PIN 13
+#define RED_LED 23		// RED_LED의 + 극에 연결된 라즈베리파이 핀
+#define YELLOW_LED 24	// YELLOW_LED의 + 극에 연결된 라즈베리파이 핀
+#define GREEN_LED 25	// GREEN_LED의 + 극에 연결된 라즈베리파이 핀
+#define BTN_PIN 13		// BUTTON에 연결된 라즈베리파이 핀
 
+/** 함수 호출시 RED, YELLOW LED를 blink */
 void on_off(int flag) {
 	//controls red & yellow LED
 	digitalWrite(RED_LED, flag);
 	digitalWrite(YELLOW_LED, !flag);
 }
 
+/** 인터럽트 발생시 실행시킬 핸들러 함수 */
 void btn_interrupt() {
 	//interrupt function
 	//turns on green LED
@@ -33,6 +35,7 @@ int main() {
 	wiringPiSetupGpio();
 	//uses broadcom pinmap
 	
+	/** LED에 연결된 각 핀들을 OUTPUT(출력) 모드로 설정 */
 	pinMode(RED_LED, OUTPUT);
 	pinMode(YELLOW_LED, OUTPUT);
 	pinMode(GREEN_LED, OUTPUT);
@@ -41,7 +44,7 @@ int main() {
 		printf("%s\n","Interrupt setup fail");
 	}
 
-	for(;;) {
+	for(;;) { // = while(1)
 		on_off(flag);
 		printf("%s\n","execute loop");
 		delay(1000);
